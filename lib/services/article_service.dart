ff9a339fec2c23bbe01db5a713de4c85efb92daa
Cyrus <74969028+Enderfire02@users.noreply.github.com>
2022-02-03 18:44:24 +0100

    Settings fait: peut être faire des hotfix sur la caçon d'implémenter les buttons

--- lib/services/article_service.dart
+++ lib/services/article_service.dart
@@ -2,10 +2,18 @@ import 'package:http/http.dart' as http;
 import 'package:mobile_app/utils/utils.dart';
 
 class ArticleService {
-  static Future<dynamic> createUser() async {
+  static Future<dynamic> createUser(title, resume, text, published, update, idUser) async {
     try {
       final response =
-          await http.post(Uri.parse('$uriApi/postArticle.php'), body: {});
+          await http.post(Uri.parse('$uriApi/postArticle.php'), body: {
+            "title": "$title",
+            "resume": "$resume",
+            "text": "$text",
+            "published": "$published",
+            "update": "$update",
+            "idUser": "$idUser"
+
+          });
 
       if (response.statusCode != 200) {
         return false;
