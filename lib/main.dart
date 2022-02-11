6ca850c843b25d987228e8b8b9aafb11bbe7611b
Feist <comptedoe@gmail.com>
2022-02-11 10:56:23 +0100

    Fix

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class MyApp extends StatelessWidget {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const CircularProgressIndicator();
           } else if (snapshot.hasData) {
-            return const AcceuilProfile();
+            return const AcceuilProfil();
           } else {
             return const LoginPage();
           }
